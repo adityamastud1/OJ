d8429e3d1f8a2bbda5bbe1730df14b89382385cc
adityamastud1 <adityamastud1@gmail.com>
2025-08-07 21:58:23 +0530

    preparing for deployement

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,25 +2,37 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/user');
 
+// Dynamically set callback URL based on environment
+const callbackURL =
+  process.env.NODE_ENV === "production"
+    ? "https://algou-oj-backend.onrender.com/auth/google/callback"
+    : "http://localhost:5000/auth/google/callback";
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: callbackURL
 }, async (accessToken, refreshToken, profile, done) => {
-  const existingUser = await User.findOne({ googleId: profile.id });
-  if (existingUser) return done(null, existingUser);
+  try {
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if (existingUser) return done(null, existingUser);
 
-  const newUser = await User.create({
-    googleId: profile.id,
-    fullName: profile.displayName,
-    email: profile.emails[0].value
-  });
-  done(null, newUser);
+    const newUser = await User.create({
+      googleId: profile.id,
+      fullName: profile.displayName,
+      email: profile.emails[0].value
+    });
+
+    done(null, newUser);
+  } catch (err) {
+    done(err, null);
+  }
 }));
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
+
 passport.deserializeUser((id, done) => {
   User.findById(id).then(user => done(null, user));
 });
